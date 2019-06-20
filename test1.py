ce6d09d7b59a04467c90c646b71b8a06ed6a3ea8
fortinetps <dyao@fortinet.com>
2019-06-20 12:37:15 -0400

    add new function to first script

--- test1.py
+++ test1.py
@@ -1,1 +1,14 @@
-print ("hello world")
+import os
+
+
+def main():
+    cwd = os.getcwd()
+    cwd = cwd + "/newfolder"
+    if not cwd is None:
+        print(cwd)
+    print(cwd)
+    print("hello world")
+
+
+if __name__ == '__main__':
+    main()
